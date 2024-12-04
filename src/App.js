edd443729085604e3daca5b7c9347cd74adbf4f5
React_Development <loveshkumar2000mahur@gmail.com>
2024-12-04 14:00:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './Textform';
+import React, {useState} from 'react';
+function App() {
+  const  [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'white';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = '';
+    }
+  }
+  return (
+    <>
+    <Navbar title= "TextForm" Features="Profile" love="Messages" Recents="Search" mode = {mode} toggleMode={toggleMode}/>
+    <div className="container my-3">
+    <Textform heading="enter the text to analyze"/>   
+    </div>
+    </>
+   ); 
+}
+
+export default App;
